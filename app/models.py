c5921dd12193164d76d4fc07c988827cf74b1dba
estherndichu <itskuijenga@gmail.com>
2021-03-08 15:44:15 +0300

    login form

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from .import login_manager
 from . import db
 from flask_login import UserMixin,current_user
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 @login_manager.user_loader
 def load_user(user_id):
